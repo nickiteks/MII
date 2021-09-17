ea8d96c7f3b605f973422fbbff7e13fcc9c47e01
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-09-17 17:05:09 +0400

    lab1

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import skfuzzy as fuzz
 import numpy as np
 
 x = np.arange(11)
+function = [1, 2, 5, 9]
 
-mfx = fuzz.trapmf(x, [1, 4, 8, 9])
+mfx = fuzz.trapmf(x, function)
 
 print(mfx)
 # 0 1 2 3 4 5 6 7 8 9 100
@@ -12,3 +13,21 @@ print(mfx)
 # 2 подходит 1
 # 7
 # 8 подходит 1 - (8-5)/(9-5)
+
+result = []
+
+for i in x:
+    if function[0] <= i <= function[3]:
+        if function[0] <= i <= function[1]:
+            result.append(1 - (function[1] - i) / (function[1] - function[0]))
+            continue
+        if function[1] <= i <= function[2]:
+            result.append(1)
+            continue
+        if function[2] <= i <= function[3]:
+            result.append(1 - (i - function[2]) / (function[3] - function[2]))
+            continue
+    else:
+        result.append(0)
+
+print(result)
