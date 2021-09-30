831da82ee8d3198976c883199d85220fe1fa39c5
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-09-30 23:37:33 +0400

    lab2

--- main.py
+++ main.py
@@ -1,37 +1,24 @@
-import skfuzzy as fuzz
-import numpy as np
-import matplotlib.pyplot as plt
-
-x = np.arange(int(input('введите кол-во патрон')))
-function = [1,6,8,100]
-
-#mfx = fuzz.trapmf(x, function)
-
-#print(mfx)
-# 0 1 2 3 4 5 6 7 8 9 100
-# 0 неподходит = 0
-# 1 подходит считаeм 1 - (2-1)/(2-1)
-# 2 подходит 1
-# 7
-# 8 подходит 1 - (8-5)/(9-5)
-
-result = []
-
-for i in x:
-    if function[0] <= i <= function[3]:
-        if function[0] <= i <= function[1]:
-            result.append(1 - (function[1] - i) / (function[1] - function[0]))
-            continue
-        if function[1] <= i <= function[2]:
+def count_cluster(x, function):
+    result = []
+    if function[0] <= x <= function[3]:
+        if function[0] <= x <= function[1]:
+            result.append(1 - (function[1] - x) / (function[1] - function[0]))
+        if function[1] <= x <= function[2]:
             result.append(1)
-            continue
-        if function[2] <= i <= function[3]:
-            result.append(1 - (i - function[2]) / (function[3] - function[2]))
-            continue
+        if function[2] <= x <= function[3]:
+            result.append(1 - (x - function[2]) / (function[3] - function[2]))
     else:
         result.append(0)
+    return result
 
-plt.plot(result)
-plt.show()
 
-print(result)
+def clustering(x):
+    print(f"Это на {count_cluster(x, [0, 25, 44, 100])} молодой возвраст")
+    print(f"Это на {count_cluster(x, [0, 44, 60, 100])} средний  возвраст")
+    print(f"Это на {count_cluster(x, [0, 60, 75, 100])} пожилой возвраст")
+    print(f"Это на {count_cluster(x, [0, 75, 90, 100])} старческий возвраст")
+
+
+trigger = True
+while trigger:
+    clustering(int(input("Введите возвраст ")))
