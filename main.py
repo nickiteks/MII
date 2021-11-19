45e1683877887701b282a95bc1196c51509aea2e
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-11-20 00:48:21 +0400

    ebat'

--- main.py
+++ main.py
@@ -1,27 +1,68 @@
 import tkinter
+import random
 from tkinter import *
+import pandas as pd
 
 import numpy as np
 from matplotlib import pyplot as plt
 
 list_functions = []
-clear_time_series = [10, 15, 17, 20, 22, 17]
+clear_time_series = [random.randint(2, 24) for _ in range(50)]
 list_time_marks = []
 graph_indexes = []
 list_tendentions = []
+prognoze_list = []
+
+
+def getNext(listing):
+    add_list = []
+    predict = listing[len(listing) - 1][1]
+    for item in listing:
+        if item[0] == predict:
+            add_list.append(item)
+    a = pd.Index(add_list)
+    print(a.value_counts())
+    print(a.value_counts().index[0])
+    return a.value_counts().index[0][1]
+
+
+def predict():
+    prognoze_list.clear()
+    for i in range(len(list_tendentions) - 2):
+        list_work = [list_tendentions[i], list_tendentions[i + 1]]
+        prognoze_list.append(list_work)
+    print(prognoze_list)
+    print(getNext(prognoze_list))
+    # Определяем лингвистическую метку будующего числа
+    graph_indexes.append(graph_indexes[len(graph_indexes) - 1] + getNext(prognoze_list))
+    # Находим максимальное опдходящее по лингвистической метке
+    max = list_functions[graph_indexes[len(graph_indexes) - 1]][0]
+
+    index = 0
+    # Берем нечеткое множество последнее в нашей метке идем по нему и находим максимальное подходящее число
+    for i in range(len(list_functions[graph_indexes[len(graph_indexes) - 1]])):
+        if list_functions[graph_indexes[len(graph_indexes) - 1]][i] > max:
+            max = list_functions[graph_indexes[len(graph_indexes) - 1]][i]
+            index = i
+
+    clear_time_series.append(index)
+    razn = (clear_time_series[len(clear_time_series) - 2] - clear_time_series[len(clear_time_series) - 1]) / \
+           clear_time_series[len(clear_time_series) - 2]
+
+    print(razn)
 
 
 def graph_tend():
     fig, ax = plt.subplots()
-    for x in range(len(list_tendentions)-1):
+    for x in range(len(list_tendentions) - 1):
         if list_tendentions[x] == list_tendentions[x + 1]:
-            plt.plot([x,x+1],[list_tendentions[x],list_tendentions[x+1]],color='g')
+            plt.plot([x, x + 1], [list_tendentions[x], list_tendentions[x + 1]], color='g')
         if list_tendentions[x + 1] == 0:
-            plt.plot([x,x+1],[list_tendentions[x],list_tendentions[x+1]],color='g')
+            plt.plot([x, x + 1], [list_tendentions[x], list_tendentions[x + 1]], color='g')
         if list_tendentions[x + 1] == 1:
-            plt.plot([x,x+1],[list_tendentions[x],list_tendentions[x+1]],color='r')
+            plt.plot([x, x + 1], [list_tendentions[x], list_tendentions[x + 1]], color='r')
         if list_tendentions[x + 1] == -1:
-            plt.plot([x,x+1],[list_tendentions[x],list_tendentions[x+1]],color='b')
+            plt.plot([x, x + 1], [list_tendentions[x], list_tendentions[x + 1]], color='b')
 
     plt.show()
 
@@ -44,7 +85,12 @@ def graph_NVK():
 
 
 def graph_ChVK():
-    plt.plot(clear_time_series)
+    list_help = clear_time_series.copy()
+    a = list_help.pop()
+    plt.plot(list_help, color='r')
+    list_help.pop()
+    list_help.append(a)
+    plt.plot(list_help, color='g')
     plt.show()
 
 
@@ -225,7 +271,7 @@ btn_time.grid(column=8, row=10)
 btn_tend = Button(window, text="Тенденции!", command=get_tendentions)
 btn_tend.grid(column=9, row=10)
 
-btn_graph_ChVK = Button(window, text="График чвр!", command=graph_ChVK)
+btn_graph_ChVK = Button(window, text="График предсказания", command=graph_ChVK)
 btn_graph_ChVK.grid(column=7, row=11)
 
 btn_graph_NVK = Button(window, text="График НВР!", command=graph_NVK)
@@ -236,5 +282,14 @@ btn_graph_tend.grid(column=9, row=11)
 
 btn_global = Button(window, text="Глобальный клик", command=global_method)
 btn_global.grid(column=8, row=9)
+
+btn_pred = Button(window, text="Прогноз!", command=predict)
+btn_pred.grid(column=7, row=7)
+
+x = np.arange(100)
+list_functions.append(count_function(x, 1, 10, 14, 100, True))
+list_functions.append(count_function(x, 1, 15, 20, 100, True))
+list_functions.append(count_function(x, 1, 21, 30, 100, True))
+listbox_update()
 
 window.mainloop()
